77ff4ea9e231d254f28fe4d118c0106e88adff87
“anar <sehar.uzair23@gmail.com>
2023-06-08 09:45:41 -0300

    completed

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -9,9 +9,9 @@ module.exports = () => {
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js',
-      database: './src/js/database.js',
-      editor: './src/js/editor.js',
-      header: './src/js/header.js'
+      // database: './src/js/database.js',
+      // editor: './src/js/editor.js',
+      // header: './src/js/header.js'
     },
     output: {
       filename: '[name].bundle.js',
@@ -21,7 +21,7 @@ module.exports = () => {
 //generate html and injectsthe bundles      
       new HtmlWebpackPlugin({
         template:'./index.html',
-        title:'JATE'
+        title:'J.A.T.E'
       }),
 // injects custom service workers
       new InjectManifest ({
@@ -33,7 +33,7 @@ module.exports = () => {
         fingerprints: false,
         inject: true,
         name: 'Just Another Text Editor',
-        short_name: 'JATE',
+        short_name: 'J.A.T.E',
         description: 'Just another text editor',
         background_color: '#225ca3',
         theme_color: '#225ca3',
@@ -64,11 +64,12 @@ module.exports = () => {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
+              plugins: ['@babel/transform-runtime']
             },
           },
         },
       ],
     },
+    stats: { children: true }
   };
 };
